7a44c29bb4d63f05639daf8357654e0d842b723c
zj <zhoujing382@gmail.com>
2024-08-22 16:25:25 +0800

    1 add

--- d2l/torch.py
+++ d2l/torch.py
@@ -1066,8 +1066,7 @@ def sequence_mask(X, valid_len, value=0):
 
     Defined in :numref:`sec_seq2seq_decoder`"""
     maxlen = X.size(1)
-    mask = torch.arange((maxlen), dtype=torch.float32,
-                        device=X.device)[None, :] < valid_len[:, None]
+    mask = torch.arange((maxlen), dtype=torch.float32, device=X.device)[None, :] < valid_len[:, None]
     X[~mask] = value
     return X
 
